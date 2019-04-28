02ed043d07b1b5eb07243b791919c71b2947f7e5
Deepak Shivamurthy <sdeepak829@hotmail.com>
2019-04-28 00:55:14 -0700

    DTOs annotated as @Value.Immutables

--- src/main/java/com/immutablesmapstruct/demo/dto/model/UserDto.java
+++ src/main/java/com/immutablesmapstruct/demo/dto/model/UserDto.java
@@ -1,10 +1,14 @@
 package com.immutablesmapstruct.demo.dto.model;
 
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Value.Style(defaults = @Value.Immutable(copy = false))
 public abstract class UserDto {
 
     public abstract int id();
     public abstract String username();
     public abstract String email();
     public abstract UserStatusDto userStatus();
-    
+
 }
--- src/main/java/com/immutablesmapstruct/demo/dto/model/UserStatusDto.java
+++ src/main/java/com/immutablesmapstruct/demo/dto/model/UserStatusDto.java
@@ -1,5 +1,9 @@
 package com.immutablesmapstruct.demo.dto.model;
 
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Value.Style(defaults = @Value.Immutable(copy = false))
 public abstract class UserStatusDto {
 
     public abstract int id();
