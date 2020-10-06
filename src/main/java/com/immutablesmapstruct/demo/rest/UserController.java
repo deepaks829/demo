fc7e41b1939e6f827a1b5be9c91fb1894545c7d9
Deepak Shivamurthy <dshivamu@cadence.com>
2020-10-06 10:12:58 -0700

    PR-4 Adding javadoc for User Controller

--- src/main/java/com/immutablesmapstruct/demo/rest/UserController.java
+++ src/main/java/com/immutablesmapstruct/demo/rest/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * User controller defining the RestAPI endpoints
+ */
 @RestController
 @RequestMapping("/users")
 public interface UserController {
